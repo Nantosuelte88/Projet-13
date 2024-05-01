84208bbfa448db20e825bdfd6dd86e2f575add84
Deborah Druine <deborahdruine@gmail.com>
2024-05-01 20:15:06 +0200

    ajout des applications lettings et profiles, separation des templates

---
+++ lettings/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Letting
+from .models import Address
+
+admin.site.register(Letting)
+admin.site.register(Address)
---
+++ profiles/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'profiles'
+
+urlpatterns = [
+    path('', views.index, name='profile_index'),
+    path('<str:username>/', views.profile, name='profile'),
+]

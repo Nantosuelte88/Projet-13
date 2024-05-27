3198a1a914a6606b8229c9dc15c5019f25bdba3a
Deborah Druine <deborahdruine@gmail.com>
2024-05-27 15:30:01 +0200

    modification settings pour deploiement

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition

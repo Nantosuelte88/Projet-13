bbe2e8133e4ff235baab05cd6ec51fa61e4795d2
Deborah Druine <deborahdruine@gmail.com>
2024-06-07 12:17:42 +0200

    clé secrete dans fichier env

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
